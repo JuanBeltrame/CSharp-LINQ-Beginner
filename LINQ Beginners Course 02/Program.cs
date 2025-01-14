fc55111ec2d6572ba86966adc0279e28116729b9
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-14 06:32:29 -0300

    UPdate

--- LINQ Beginners Course 02/Program.cs
+++ LINQ Beginners Course 02/Program.cs
@@ -1,1 +1,12 @@
-﻿int[] arregloEnteros = [1, 2, 10, 3, 2, 55, 666, 18, 2, 100, 2, 41];
+﻿// LINQ to Objects
+int[] arregloEnteros = { 1, 2, 10, 3, 2, 55, 666, 18, 2, 100, 2, 41 };
+
+
+IEnumerable<int> orderedNumbers = from d in arregloEnteros
+                                  orderby d
+                                  select d;
+
+// Query Syntax
+foreach (var a in orderedNumbers)
+    Console.WriteLine(a);
+
