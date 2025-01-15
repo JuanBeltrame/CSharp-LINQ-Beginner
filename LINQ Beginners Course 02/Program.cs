236a104f5dbc3beedf95ff1f6d779e3c55e2375d
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-15 06:01:47 -0300

    Update LINQ

--- LINQ Beginners Course 02/Program.cs
+++ LINQ Beginners Course 02/Program.cs
@@ -1,12 +1,47 @@
 ﻿// LINQ to Objects
 int[] arregloEnteros = { 1, 2, 10, 3, 2, 55, 666, 18, 2, 100, 2, 41 };
-
-
-IEnumerable<int> orderedNumbers = from d in arregloEnteros
-                                  orderby d
-                                  select d;
+Complejo[] arregloComplejos = [new Complejo(1, "Patito"), 
+                               new Complejo(666, "Diablito"), 
+                               new Complejo(5, "Perro")];
 
 // Query Syntax
-foreach (var a in orderedNumbers)
-    Console.WriteLine(a);
+IEnumerable<int> queryOne = from d in arregloEnteros
+                            orderby d
+                            select d;
+
+IEnumerable<int> queryTwo = from d in arregloEnteros
+                            where d == 2
+                            orderby d
+                            select d;
+
+IEnumerable<int> queryThree = from d in arregloEnteros
+                              where d == 2 || d == 10
+                              orderby d
+                              select d;
+
+
+
+// Print Queries
+Console.Write("Query One: ");
+foreach (var a in queryOne)
+    Console.Write(a);
+Console.WriteLine();
+
+Console.Write("Query Two: ");
+foreach (var b in queryTwo)
+    Console.Write(b);
+Console.WriteLine();
+
+Console.Write("Query Three: ");
+foreach (var c in queryThree)
+    Console.Write(c);
+
+
+public class Complejo(int Numero, string Cadena)
+{
+    public int Numero { get; set; } = Numero;
+    public string? Cadena { get; set; } = Cadena;
+
+    public override string ToString() =>  $"Numero: {Numero}, Cadena: {Cadena}";
+}
 
