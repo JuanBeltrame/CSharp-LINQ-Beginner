766258c8df00f573d3885db40c9755b5fa3b930b
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-16 05:59:25 -0300

    UPdate

--- LINQ Beginners Course 02/Program.cs
+++ LINQ Beginners Course 02/Program.cs
@@ -1,10 +1,15 @@
-﻿// LINQ to Objects
-int[] arregloEnteros = { 1, 2, 10, 3, 2, 55, 666, 18, 2, 100, 2, 41 };
-Complejo[] arregloComplejos = [new Complejo(1, "Patito"), 
-                               new Complejo(666, "Diablito"), 
-                               new Complejo(5, "Perro")];
+﻿//------------------- LINQ to Objects -------------------
+using System.ComponentModel.Design;
 
-// Query Syntax
+int[] arregloEnteros = { 1, 2, 10, 3, 2, 55, 666, 18, 2, 100, 2, 41 };
+
+Complejo[] arregloComplejos = [new Complejo(1, "Patito"),
+                               new Complejo(666, "Diablito"),
+                               new Complejo(5, "Perro"),
+                               new Complejo(10, "Ave"),
+                               new Complejo(66,"Foca")];
+
+//------------------- Query Syntax -------------------
 IEnumerable<int> queryOne = from d in arregloEnteros
                             orderby d
                             select d;
@@ -19,7 +24,16 @@ IEnumerable<int> queryThree = from d in arregloEnteros
                               orderby d
                               select d;
 
-// Print Queries
+var queryFour = (from d in arregloComplejos
+              where d.Cadena == "Diablito"
+              select d).FirstOrDefault();
+
+var queryFive = (from d in arregloComplejos
+                 orderby d.Numero
+                 select d);
+
+
+//------------------- Display Queries -------------------
 Console.Write("Query One: ");
 foreach (var a in queryOne)
     Console.Write(a);
@@ -34,6 +48,11 @@ Console.Write("Query Three: ");
 foreach (var c in queryThree)
     Console.Write(c);
 
+Console.WriteLine(queryFour?.ToString());
+
+Console.WriteLine("Query Five: ");
+foreach(var element in queryFive)
+    Console.WriteLine(element.ToString());
 
 public class Complejo(int Numero, string Cadena)
 {
---
+++ LINQ Beginners Course 03/Program.cs
@@ -0,0 +1,41 @@
+﻿//------------------- LINQ to Objects -------------------
+var beers = new List<Beer> {
+    new Beer { Name = "Sin Alcohol", brand = "Quilmes", AlcoholByVolume = 5.2, BreweryId = 1 },
+    new Beer { Name = "IPA", brand = "Brahama", AlcoholByVolume = 4.9, BreweryId = 1 },
+    new Beer { Name = "Negra", brand = "Stella Artois", AlcoholByVolume = 6.6, BreweryId = 1 },
+    new Beer { Name = "Roja", brand = "Heineken", AlcoholByVolume = 9.0, BreweryId = 2 },
+    new Beer { Name = "Rubia", brand = "Andes Origen", AlcoholByVolume = 8.5, BreweryId = 2 },
+    };
+
+
+//------------------- Query Syntax -------------------
+var breweries = from d in beers
+                select d.Name;
+
+var breweries2 = from d in beers
+                 select new { d.Name, d.brand };
+
+var breweires3 = from b in beers
+                 where b.brand == "Quilmes"
+                 select new { b.Name, b.brand };
+
+
+//------------------- Display Queries -------------------
+foreach (var a in breweries)
+    Console.WriteLine(a);
+Console.WriteLine();
+
+foreach (var item in breweries2)
+    Console.WriteLine(item.brand);
+
+foreach (var item in breweires3)
+    Console.WriteLine(item.brand);
+
+
+public class Beer
+{
+    public string Name { get; set; }
+    public string brand { get; set; }
+    public double AlcoholByVolume { get; set; }
+    public int BreweryId { get; set; }
+}
